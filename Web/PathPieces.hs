3fc4ba534f0149638f8ae1638895646293692846
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2015-10-04 01:48:34 +0300

    Use default PathPiece instances for some basic types

--- Web/PathPieces.hs
+++ Web/PathPieces.hs
@@ -6,13 +6,11 @@ module Web.PathPieces
     , showToPathPiece
     ) where
 
-import Data.Int (Int8, Int16, Int32, Int64)
-import Data.Word (Word, Word8, Word16, Word32, Word64)
+import Data.Int
+import Data.Word
 import qualified Data.Text as S
 import qualified Data.Text.Lazy as L
-import qualified Data.Text.Read
 import Data.Time (Day)
-import Control.Exception (assert)
 import Text.Read (readMaybe)
 
 import Web.HttpApiData
@@ -26,85 +24,26 @@ class PathPiece s where
   default toPathPiece :: ToHttpApiData s => s -> S.Text
   toPathPiece = toUrlPiece
 
+instance PathPiece Bool
+instance PathPiece Double
+instance PathPiece Float
+instance PathPiece Int
+instance PathPiece Int8
+instance PathPiece Int16
+instance PathPiece Int32
+instance PathPiece Int64
+instance PathPiece Word
+instance PathPiece Word8
+instance PathPiece Word16
+instance PathPiece Word32
+instance PathPiece Word64
+instance PathPiece String
+instance PathPiece S.Text
+instance PathPiece L.Text
+
 instance PathPiece () where
     fromPathPiece t = if t == "_" then Just () else Nothing
     toPathPiece () = "_"
-
-instance PathPiece String where
-    fromPathPiece = Just . S.unpack
-    toPathPiece = S.pack
-
-instance PathPiece S.Text where
-    fromPathPiece = Just
-    toPathPiece = id
-
-instance PathPiece L.Text where
-    fromPathPiece = Just . L.fromChunks . return
-    toPathPiece = S.concat . L.toChunks
-
-parseIntegral :: (Integral a, Bounded a, Ord a) => S.Text -> Maybe a
-parseIntegral s = n
-    where
-    n = case Data.Text.Read.signed Data.Text.Read.decimal s of
-        Right (i, "") | i <= top && i >= bot -> Just (fromInteger i)
-        _ -> Nothing
-    Just witness = n
-    top = toInteger (maxBound `asTypeOf` witness)
-    bot = toInteger (minBound `asTypeOf` witness)
-
-instance PathPiece Integer where
-    fromPathPiece s =
-        case Data.Text.Read.signed Data.Text.Read.decimal s of
-            Right (i, "") -> Just i
-            _ -> Nothing
-    toPathPiece = S.pack . show
-
-instance PathPiece Int where
-    fromPathPiece = parseIntegral
-    toPathPiece = S.pack . show
-
-instance PathPiece Int8 where
-    fromPathPiece = parseIntegral
-    toPathPiece = S.pack . show
-
-instance PathPiece Int16 where
-    fromPathPiece = parseIntegral
-    toPathPiece = S.pack . show
-
-instance PathPiece Int32 where
-    fromPathPiece = parseIntegral
-    toPathPiece = S.pack . show
-
-instance PathPiece Int64 where
-    fromPathPiece = parseIntegral
-    toPathPiece = S.pack . show
-
-instance PathPiece Word where
-    fromPathPiece = parseIntegral
-    toPathPiece = S.pack . show
-
-instance PathPiece Word8 where
-    fromPathPiece = parseIntegral
-    toPathPiece = S.pack . show
-
-instance PathPiece Word16 where
-    fromPathPiece = parseIntegral
-    toPathPiece = S.pack . show
-
-instance PathPiece Word32 where
-    fromPathPiece = parseIntegral
-    toPathPiece = S.pack . show
-
-instance PathPiece Word64 where
-    fromPathPiece = parseIntegral
-    toPathPiece = S.pack . show
-
-instance PathPiece Bool where
-    fromPathPiece t =
-        case filter (null . snd) $ reads $ S.unpack t of
-            (a, s):_ -> assert (null s) (Just a)
-            _        -> Nothing
-    toPathPiece = S.pack . show
 
 instance PathPiece Day where
     fromPathPiece t =
