7a8359733b584af2a838f1eec990f1bef7d989ee
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2015-10-07 01:01:51 +0300

    Add tests for case insensitivity, also refactor tests a bit

--- test/main.hs
+++ test/main.hs
@@ -5,6 +5,7 @@ module Main where
 import Control.Applicative
 
 import Data.Int
+import Data.Char
 import Data.Word
 import Data.Time
 import qualified Data.Text as T
@@ -38,37 +39,67 @@ main = hspec spec
 (<=>) :: Eq a => (a -> b) -> (b -> Maybe a) -> a -> Bool
 (f <=> g) x = g (f x) == Just x
 
+data Proxy a = Proxy
+
+checkPathPiece :: forall a. (Eq a, PathPiece a, Show a, Arbitrary a) => Proxy a -> String -> Spec
+checkPathPiece _ name = prop ("toPathPiece <=> fromPathPiece " ++ name) (toPathPiece <=> fromPathPiece :: a -> Bool)
+
+checkPathMultiPiece :: forall a. (Eq a, PathMultiPiece a, Show a, Arbitrary a) => Proxy a -> String -> Spec
+checkPathMultiPiece _ name = prop ("toPathMultiPiece <=> fromPathMultiPiece " ++ name) (toPathMultiPiece <=> fromPathMultiPiece :: a -> Bool)
+
+data RandomCase a = RandomCase [Bool] a
+
+instance PathPiece a => Show (RandomCase a) where
+  show rc@(RandomCase _ x) = show (toPathPiece rc) ++ " (original: " ++ show (toPathPiece x) ++ ")"
+
+instance Eq a => Eq (RandomCase a) where
+  RandomCase _ x == RandomCase _ y = x == y
+
+instance Arbitrary a => Arbitrary (RandomCase a) where
+  arbitrary = liftA2 RandomCase nonempty arbitrary
+    where
+      nonempty = liftA2 (:) arbitrary arbitrary
+
+instance PathPiece a => PathPiece (RandomCase a) where
+  toPathPiece (RandomCase us x) = T.pack (zipWith (\u -> if u then toUpper else toLower) (cycle us) (T.unpack (toPathPiece x)))
+  fromPathPiece s = RandomCase [] <$> fromPathPiece s
+
+-- | Check case insensitivity for @fromPathPiece@.
+checkPathPieceI :: forall a. (Eq a, PathPiece a, Show a, Arbitrary a) => Proxy a -> String -> Spec
+checkPathPieceI _ = checkPathPiece (Proxy :: Proxy (RandomCase a))
+
 spec :: Spec
 spec = do
   describe "PathPiece" $ do
-    prop "toPathPiece <=> fromPathPiece ()"           $ \(p :: ())        -> (toPathPiece <=> fromPathPiece) p
-    prop "toPathPiece <=> fromPathPiece Char"         $ \(p :: Char)      -> (toPathPiece <=> fromPathPiece) p
-    prop "toPathPiece <=> fromPathPiece Bool"         $ \(p :: Bool)      -> (toPathPiece <=> fromPathPiece) p
-    prop "toPathPiece <=> fromPathPiece Ordering"     $ \(p :: Ordering)  -> (toPathPiece <=> fromPathPiece) p
-    prop "toPathPiece <=> fromPathPiece Int"          $ \(p :: Int)       -> (toPathPiece <=> fromPathPiece) p
-    prop "toPathPiece <=> fromPathPiece Int8"         $ \(p :: Int8)      -> (toPathPiece <=> fromPathPiece) p
-    prop "toPathPiece <=> fromPathPiece Int16"        $ \(p :: Int16)     -> (toPathPiece <=> fromPathPiece) p
-    prop "toPathPiece <=> fromPathPiece Int32"        $ \(p :: Int32)     -> (toPathPiece <=> fromPathPiece) p
-    prop "toPathPiece <=> fromPathPiece Int64"        $ \(p :: Int64)     -> (toPathPiece <=> fromPathPiece) p
-    prop "toPathPiece <=> fromPathPiece Integer"      $ \(p :: Integer)   -> (toPathPiece <=> fromPathPiece) p
-    prop "toPathPiece <=> fromPathPiece Word"         $ \(p :: Word)      -> (toPathPiece <=> fromPathPiece) p
-    prop "toPathPiece <=> fromPathPiece Word8"        $ \(p :: Word8)     -> (toPathPiece <=> fromPathPiece) p
-    prop "toPathPiece <=> fromPathPiece Word16"       $ \(p :: Word16)    -> (toPathPiece <=> fromPathPiece) p
-    prop "toPathPiece <=> fromPathPiece Word32"       $ \(p :: Word32)    -> (toPathPiece <=> fromPathPiece) p
-    prop "toPathPiece <=> fromPathPiece Word64"       $ \(p :: Word64)    -> (toPathPiece <=> fromPathPiece) p
-    prop "toPathPiece <=> fromPathPiece Word64"       $ \(p :: Word64)    -> (toPathPiece <=> fromPathPiece) p
-    prop "toPathPiece <=> fromPathPiece String"       $ \(p :: String)    -> (toPathPiece <=> fromPathPiece) p
-    prop "toPathPiece <=> fromPathPiece Text.Strict"  $ \(p :: T.Text)    -> (toPathPiece <=> fromPathPiece) p
-    prop "toPathPiece <=> fromPathPiece Text.Lazy"    $ \(p :: L.Text)    -> (toPathPiece <=> fromPathPiece) p
-    prop "toPathPiece <=> fromPathPiece Day"          $ \(p :: Day)       -> (toPathPiece <=> fromPathPiece) p
-    prop "toPathPiece <=> fromPathPiece Version"      $ \(p :: Version)   -> (toPathPiece <=> fromPathPiece) p
+    checkPathPiece  (Proxy :: Proxy ())        "()"
+    checkPathPiece  (Proxy :: Proxy Char)      "Char"
+    checkPathPieceI (Proxy :: Proxy Bool)      "Bool"
+    checkPathPieceI (Proxy :: Proxy Ordering)  "Ordering"
+    checkPathPiece  (Proxy :: Proxy Int)       "Int"
+    checkPathPiece  (Proxy :: Proxy Int8)      "Int8"
+    checkPathPiece  (Proxy :: Proxy Int16)     "Int16"
+    checkPathPiece  (Proxy :: Proxy Int32)     "Int32"
+    checkPathPiece  (Proxy :: Proxy Int64)     "Int64"
+    checkPathPiece  (Proxy :: Proxy Integer)   "Integer"
+    checkPathPiece  (Proxy :: Proxy Word)      "Word"
+    checkPathPiece  (Proxy :: Proxy Word8)     "Word8"
+    checkPathPiece  (Proxy :: Proxy Word16)    "Word16"
+    checkPathPiece  (Proxy :: Proxy Word32)    "Word32"
+    checkPathPiece  (Proxy :: Proxy Word64)    "Word64"
+    checkPathPiece  (Proxy :: Proxy String)    "String"
+    checkPathPiece  (Proxy :: Proxy T.Text)    "Text.Strict"
+    checkPathPiece  (Proxy :: Proxy L.Text)    "Text.Lazy"
+    checkPathPiece  (Proxy :: Proxy Day)       "Day"
+    checkPathPiece  (Proxy :: Proxy Version)   "Version"
 
-    prop "toPathPiece <=> fromPathPiece Maybe String"        $ \(p::Maybe String)          -> (toPathPiece <=> fromPathPiece) p
-    prop "toPathPiece <=> fromPathPiece Either Integer Text" $ \(p::Either Integer T.Text) -> (toPathPiece <=> fromPathPiece) p
+    checkPathPiece  (Proxy :: Proxy (Maybe String))            "Maybe String"
+    checkPathPieceI (Proxy :: Proxy (Maybe Integer))           "Maybe Integer"
+    checkPathPiece  (Proxy :: Proxy (Either Integer T.Text))   "Either Integer Text"
+    checkPathPieceI (Proxy :: Proxy (Either Version Day))      "Either Version Day"
 
   describe "PathMultiPiece" $ do
-    prop "toPathMultiPiece <=> fromPathMultiPiece String" $ \(p::[String]) -> (toPathMultiPiece <=> fromPathMultiPiece) p
-    prop "toPathMultiPiece <=> fromPathMultiPiece Text"   $ \(p::[T.Text]) -> (toPathMultiPiece <=> fromPathMultiPiece) p
+    checkPathMultiPiece (Proxy :: Proxy [String])   "[String]"
+    checkPathMultiPiece (Proxy :: Proxy [T.Text])   "[Text]"
 
   it "bad integers are rejected" $ do
     fromPathPiece (T.pack "123hello") `shouldBe` (Nothing :: Maybe Int)
